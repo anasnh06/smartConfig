405eda2855360d16b03f7a45d00fbc15c5691d98
Nouhassi Anas <nouhassi@localhost.localdomain>
2025-05-19 16:18:30 +0100

    session

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core import settings
 
-print("✅ DATABASE_URL chargée:", settings.database_url)
+
 engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
